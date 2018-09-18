d1be5ca61dcad4b5b77df466407df53bb3c7c7b1
quoioln <quoipro94@gmail.com>
2018-09-18 22:38:19 +0700

    Initial commit

---
+++ src/main/java/com/vpquoi/demo/springsecurity/SpringSecuritySampleApplication.java
@@ -0,0 +1,12 @@
+package com.vpquoi.demo.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritySampleApplication.class, args);
+    }
+}
